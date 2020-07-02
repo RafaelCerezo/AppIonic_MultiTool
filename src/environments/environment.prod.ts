c559a951afe57ce96bd2ff830d9614eae7370625
Rafa <rafaelcerezo@tequipo.com>
2020-07-02 17:49:26 +0200

    FireBase configuration variable added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAHfb_WGCs9tuLbkonFs2zhA3UQg30_J14",
+    authDomain: "multitoolapp-db.firebaseapp.com",
+    databaseURL: "https://multitoolapp-db.firebaseio.com",
+    projectId: "multitoolapp-db",
+    storageBucket: "multitoolapp-db.appspot.com",
+    messagingSenderId: "70432120796",
+    appId: "1:70432120796:web:f9e5257e50973622ef7919",
+    measurementId: "G-PC4L03ENTT"
+  }
 };

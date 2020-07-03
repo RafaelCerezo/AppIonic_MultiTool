2db7bbc027cb803304c4dc4b0c8c4c6f695150f6
Mario Barbero <elbarbero400@hotmail.com>
2020-07-03 18:03:41 +0200

    tabs basicas

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,8 +4,23 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomePage,
+    children: [
+      {
+        path: 'inicio',
+        loadChildren: () => import('../inicio/inicio.module').then(m => m.InicioPageModule)
+      },
+      {
+        path: 'eventos',
+        loadChildren: () => import('../eventos/eventos.module').then(m => m.EventosPageModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'home/inicio',
+    pathMatch: 'full'
   }
 ];
 
@@ -13,4 +28,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }

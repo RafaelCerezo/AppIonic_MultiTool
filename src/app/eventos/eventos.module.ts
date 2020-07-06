9f3f322a29986a8e88a58cd3a58d396a5edc5b31
Mario Barbero <elbarbero400@hotmail.com>
2020-07-06 17:31:59 +0200

    Intentando poner el calendario en español

--- src/app/eventos/eventos.module.ts
+++ src/app/eventos/eventos.module.ts
@@ -8,12 +8,15 @@ import { EventosPageRoutingModule } from './eventos-routing.module';
 
 import { EventosPage } from './eventos.page';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EventosPageRoutingModule
+    EventosPageRoutingModule,
+    NgCalendarModule
   ],
   declarations: [EventosPage]
 })
